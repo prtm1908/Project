ee94167aaf80113a63f465238b05adfa4e0a14ba
Pratham Batra <124366564+prtm1908@users.noreply.github.com>
2024-07-02 23:04:57 +0530

    6 months

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById("passwordForm").addEventListener("submit", function(even
     var enteredPassword = document.getElementById("pwd").value;
 
     // Hardcoded stored password (replace this with your actual stored password)
-    var storedPassword = "batra";
+    var storedPassword = "sahi password";
 
     // Compare entered password with stored password
     if (enteredPassword.toLowerCase() === storedPassword) {
